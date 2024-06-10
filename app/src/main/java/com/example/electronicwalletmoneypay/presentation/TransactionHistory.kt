2155d7b3dbdb2130005e839408d6a80fb8a84c7e
HieuNMFrovis <hieunm.frovis@gmail.com>
2024-06-10 17:57:06 +0700

    UI screen Transaction History, Profile, Notifications

---
+++ app/src/main/java/com/example/electronicwalletmoneypay/presentation/TransactionHistory.kt
@@ -0,0 +1,12 @@
+package com.example.electronicwalletmoneypay.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.electronicwalletmoneypay.R
+
+class TransactionHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction_history)
+    }
+}
