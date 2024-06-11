286b3f8c16598a7191f055c2492c8f6c31d728c4
HieuNMFrovis <hieunm.frovis@gmail.com>
2024-06-11 17:20:15 +0700

    UI Setting , Language , recharge

---
+++ app/src/main/java/com/example/electronicwalletmoneypay/presentation/Language.kt
@@ -0,0 +1,12 @@
+package com.example.electronicwalletmoneypay.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.electronicwalletmoneypay.R
+
+class Language : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
